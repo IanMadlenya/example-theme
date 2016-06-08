d19f43f214e5b9d7199fbfa679bd002256612df5
Florian <florian@yootheme.com>
2016-06-08 17:54:23 +0200

    simplifications

--- index.php
+++ index.php
@@ -1,31 +1,15 @@
 <?php
 
-/**
- * Configuration
- */
 return [
 
-    /**
-     * Unique name that identifies your theme.
-     */
     'name' => 'example-theme',
 
-    /**
-     * Define menu positions.
-     * Will be listed in the backend so that users
-     * can assign menus to these positions.
-     */
     'menus' => [
 
         'main' => 'Main',
 
     ],
 
-    /**
-     * Define widget positions.
-     * will be listed in the backend so that users
-     * can publish widgets in these positions.
-     */
     'positions' => [
 
         'sidebar' => 'Sidebar',
@@ -45,12 +29,6 @@ return [
 
     ],
 
-    /**
-     * Define theme configuration.
-     * This is the theme's default configuration. During run-time, they will be merged with
-     * settings the user has set in the backend. The default configuration can therefore
-     * be overwritten.
-     */
     'config' => [
         'navbar_sticky' => false
     ],
